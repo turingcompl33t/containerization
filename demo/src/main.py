34dfbff07a3c12bdfd0f4306077bf1bc0739608d
turingcompl33t <kdotterrer@gmail.com>
2022-08-19 05:18:51 -0400

    updates

--- demo/src/main.py
+++ demo/src/main.py
@@ -1,8 +1,23 @@
+import os
 import sys
+from typing import List
+
+def ls() -> List[str]:
+    """List the contents of the current directory."""
+    contents = []
+    for path in os.listdir(os.getcwd()):
+        if os.path.isfile(path):
+            contents.append(f"F {path}")
+        elif os.path.isdir(path):
+            contents.append(f"D {path}")
+        else:
+            contents.append(f"U {path}")
+    return contents
 
 def main() -> int:
-    with open("out.txt", "w") as f:
-        f.write(f"{'a'*8}\n")
+    contents = ls()
+    with open("ls.txt", "w") as f:
+        f.write('\n'.join(contents) + '\n')
     return 0
 
 if __name__ == "__main__":
